39511fc2a51fb7880c28f6288c0d3926f511d7e1
mbimbij <joseph.mbimbi@gmail.com>
2021-09-04 22:11:43 +0200

    Add a CFN template that creates a Beanstalk environment

--- src/main/java/com/example/cloudnativeweekjavaedition/CloudNativeWeekJavaEditionApplication.java
+++ src/main/java/com/example/cloudnativeweekjavaedition/CloudNativeWeekJavaEditionApplication.java
@@ -1,13 +1,19 @@
 package com.example.cloudnativeweekjavaedition;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudNativeWeekJavaEditionApplication {
+public class CloudNativeWeekJavaEditionApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudNativeWeekJavaEditionApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
 }
