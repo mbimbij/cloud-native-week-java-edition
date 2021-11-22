e78431fbf741c1bcf6cd24ee98686ddb35375b84
mbimbij <joseph.mbimbi@gmail.com>
2021-11-22 06:13:08 +0100

    Explicit namespace for EFK ECK local installation

--- infra/kube-local/efk/eck/install-eck.sh
+++ infra/kube-local/efk/eck/install-eck.sh
@@ -10,9 +10,9 @@ helm repo add elastic https://helm.elastic.co
 helm repo update
 helm upgrade -i elastic-operator elastic/eck-operator -n elastic-system --create-namespace
 
-k apply -f elasticsearch-eck.yml
-k apply -f kibana-eck.yml
+k -n logging apply -f elasticsearch-eck.yml
+k -n logging apply -f kibana-eck.yml
 sleep 10
-elasticpassword=$(kubectl get secret quickstart-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode; echo)
+elasticpassword=$(k -n logging  get secret quickstart-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode; echo)
 echo "elasticpassword: $elasticpassword"
 helm -n logging upgrade -i fluent-bit fluent/fluent-bit -f fluentbit-values-eck.yml --set elasticpassword=$elasticpassword
